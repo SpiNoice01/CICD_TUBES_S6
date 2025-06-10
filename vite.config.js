25c05c27dc6813cac749dd41d9a1e0828de4597b
SpiNoice01 <fadhilaagil72@gmail.com>
2025-06-10 13:32:23 +0700

    refactor(vite): simplify configuration by removing unnecessary server and build options

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,11 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
-export default defineConfig(({ command }) => ({
-    base: command === "serve" ? "" : "/build/",
-    server: {
-        host: "0.0.0.0",
-        port: 5173,
-        strictPort: true,
-        hmr: {
-            host: "10.0.0.6",
-        },
-    },
-    build: {
-        manifest: true,
-        outDir: "public/assets",
-        emptyOutDir: true,
-        rollupOptions: {
-            input: ["resources/css/app.css", "resources/js/app.js"],
-        },
-    },
+export default defineConfig({
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
-}));
+});
