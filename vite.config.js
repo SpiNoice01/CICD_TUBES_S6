529b167eae26ee80dbb827db3aaf602385d2961b
SpiNoice01 <fadhilaagil72@gmail.com>
2025-06-10 11:12:59 +0700

    feat(deploy): update Vite configuration for dynamic base path and build options

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+    base: command === "serve" ? "" : "/build/",
     server: {
         host: "0.0.0.0",
         port: 5173,
@@ -10,10 +11,18 @@ export default defineConfig({
             host: "10.0.0.6",
         },
     },
+    build: {
+        manifest: true,
+        outDir: "public/build",
+        emptyOutDir: true,
+        rollupOptions: {
+            input: ["resources/css/app.css", "resources/js/app.js"],
+        },
+    },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
-});
+}));
